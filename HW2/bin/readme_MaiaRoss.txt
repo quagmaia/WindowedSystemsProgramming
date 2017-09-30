d6b52eb8d8970d378996b6fc08391e883106d318
Maia Ross <rainbowroach@gmail.com>
2017-09-30 10:48:54 -0400

    hw3 basically done!!!

---
+++ HW2/bin/readme_MaiaRoss.txt
@@ -0,0 +1,6 @@
+Maia Ross
+
+Both assigned widgets are in the same program.
+
+javac Start.java
+java Start
---
+++ HW3/src/Start.java
@@ -0,0 +1,51 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.*;
+
+public class Start extends Application{
+	public static void main(String[] args) {
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage stage){
+		stage.setTitle("Maia Ross");
+		GridPane pane = new GridPane();
+		
+		//pane.setGridLinesVisible(true);
+		
+		//just gonna add the outer bit here
+		TabPane tabs = new TabPane();
+		pane.setHgrow(tabs,Priority.ALWAYS);
+		pane.setVgrow(tabs,Priority.ALWAYS);
+		pane.add(tabs, 0, 0, 2, 2);
+		
+		Tab first = new Tab("Transcode");
+		first.setContent(new Transcode());
+		tabs.getTabs().add(first);
+		
+		Pane filler = new Pane();
+		pane.setHgrow(filler, Priority.ALWAYS);
+		pane.setVgrow(filler, Priority.ALWAYS);
+		pane.add(filler, 0, 2);
+		
+		Button add = new Button("Add Transcode");
+		pane.add(add, 1, 5, 1, 1);
+		
+		add.setOnAction(
+				(event) -> {
+					Tab t = new Tab("New Transcode");
+					t.setContent(new Transcode());;
+					tabs.getTabs().add(t);
+				}
+		);
+		
+		
+		stage.setScene(new Scene(pane,400,400));
+		stage.show();
+	}
+
+}
